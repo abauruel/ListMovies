50c91362777dd29c83ddd7976b91f7f7e3510a5e
abauruel <abauruel@gmail.com>
2021-03-18 12:22:06 -0300

    refactor: :art: splitted in components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+type Movies = {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+type GenreProps ={
+  title: string;
+}
+type ContentProps = {
+  movies: Movies[],
+  selectedGenre: GenreProps
+}
+export function Content({movies, selectedGenre}: ContentProps) {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss'
+import { Button } from '../components/Button';
+import { useState } from 'react';
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+type SideBarProps = {
+  genres: GenreProps[],
+  selectedGenreId: number,
+  handleClickButton(id: number): void
+}
+
+export function SideBar ( {genres, selectedGenreId,handleClickButton}: SideBarProps) {
+  
+
+  return(
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+
+  
+  )
 }
